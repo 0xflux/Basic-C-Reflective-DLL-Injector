d6e9fd1434581b03894ff9058e00167c32ab66fa
Ian G <49762827+the-wandering-photon@users.noreply.github.com>
2024-01-07 11:08:38 +0000

    Revert "refactor"
    This reverts commit 54d7d5bde9be60fc91fa982f7133b5ef506cd14e.

--- bootstrapper.h
+++ bootstrapper.h
@@ -1,16 +1,8 @@
 #ifndef BOOTSTRAPPER_H
 #define BOOTSTRAPPER_H
 
-#include <windows.h>
-#include "dll_injector.h"
-
 // Function markers for bootstrapper code boundaries
 void start_of_injectable_code();
 void end_of_injectable_code();
-void perform_base_relocation(LDLL_INFO dll_info, PIMAGE_NT_HEADERS nt);
-void load_imports(LDLL_INFO dll_info, PIMAGE_NT_HEADERS nt);
-void call_tls_callbacks(LDLL_INFO dll_info, PIMAGE_NT_HEADERS nt);
-void execute_entry_point(LDLL_INFO dll_info, PIMAGE_NT_HEADERS nt);
-void realign_pe(LDLL_INFO dll_info);
 
 #endif
--- dll_injector.h
+++ dll_injector.h
@@ -8,13 +8,9 @@
  * Structure to hold various pointers and flags related to a PE (Portable Executable) file.
  * This includes pointers to functions like GetProcAddress() and LoadLibraryA()
  */
-
-typedef HMODULE (WINAPI *LoadLibraryAFunc)(LPCSTR lpLibFileName);
-typedef FARPROC (WINAPI *GetProcAddressFunc)(HMODULE hModule, LPCSTR lpProcName);
-
 typedef struct _DLL_INFO {
-    GetProcAddressFunc get_process_addr;
-    LoadLibraryAFunc load_library_a_addr;
+    LPVOID get_process_addr;
+    LPVOID load_library_a_addr;
     LPVOID base;
     BOOL base_relocation_required;
 } DLL_INFO, *LDLL_INFO;
@@ -23,5 +19,6 @@ typedef struct _DLL_INFO {
 LPVOID load_dll_from_resource(const char *dll_path, int resourceID);
 LPVOID read_dll_from_file(char *FileName);
 HANDLE find_process_and_get_handle(char *process_name);
+void realign_pe(LDLL_INFO pe);
 
 #endif // DLL_INJECTOR_H
